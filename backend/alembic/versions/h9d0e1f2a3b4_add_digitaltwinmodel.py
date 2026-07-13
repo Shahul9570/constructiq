fedf41dda82cb41593e2cfea155aca8928686ed2
Shahul <164329826+Shahul9570@users.noreply.github.com>
2026-07-13 12:44:22 +0530

    fix: resolve alembic revision not found error on deployment

---
+++ backend/alembic/versions/h9d0e1f2a3b4_add_digitaltwinmodel.py
@@ -0,0 +1,23 @@
+"""Add DigitalTwinModel
+
+Revision ID: h9d0e1f2a3b4
+Revises: g8c9d0e1f2a3
+Create Date: 2026-07-13 10:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'h9d0e1f2a3b4'
+down_revision = 'g8c9d0e1f2a3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
